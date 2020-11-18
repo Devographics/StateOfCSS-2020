81cd74fad9f73abcfb4a178818736978babc4edf
SachaG <358832+SachaG@users.noreply.github.com>
2020-11-18 11:20:01 +0900

    Refactor gatsby_node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,178 +1,16 @@
-const fs = require('fs')
 const path = require('path')
-const yaml = require('js-yaml')
-const _ = require('lodash')
-const indentString = require('indent-string')
-const { computeSitemap } = require('./node_src/sitemap/index.js')
-const { omit } = require('lodash')
-
-const config = yaml.safeLoad(fs.readFileSync('./config/config.yml', 'utf8'))
+const { createPagesSingleLoop, createPagesTwoLoops } = require('./node_src/create_pages')
 
 require('dotenv').config({
     path: `.env`,
 })
 
-const localesQuery = `
-query {
-    surveyApi {
-        locales(contexts: [${config.translationContexts.join(', ')}]) {
-            completion
-            id
-            label
-            strings {
-                key
-                t
-                context
-                fallback
-            }
-            translators
-        }
-    }
-}
-`
-
-const rawSitemap = yaml.safeLoad(fs.readFileSync('./config/raw_sitemap.yml', 'utf8'))
-// const locales = yaml.safeLoad(fs.readFileSync('./config/locales.yml', 'utf8'))
-
-const getLocalizedPath = (path, locale) => (locale ? `/${locale.id}${path}` : path)
-
-// locales without the strings, to avoid loading every locale's dictionnary in memory
-const getCleanLocales = (locales) => locales.map((l) => omit(l, ['strings']))
-
-const getPageContext = (page) => {
-    const context = omit(page, ['path', 'children'])
-    context.basePath = page.path
-
-    return {
-        ...context,
-        ...page.data,
-    }
-}
-
-const createBlockPages = (page, context, createPage, locales) => {
-    const blocks = page.blocks
-    if (!Array.isArray(blocks) || blocks.length === 0) {
-        return
-    }
-
-    blocks.forEach((block) => {
-        // allow for specifying explicit pageId in block definition
-        if (!block.pageId) {
-            block.pageId = page.id
-        }
-        locales.forEach((locale) => {
-            const blockPage = {
-                path: getLocalizedPath(block.path, locale),
-                component: path.resolve(`./src/core/share/ShareBlockTemplate.js`),
-                context: {
-                    ...context,
-                    redirect: `${getLocalizedPath(page.path, locale)}#${block.id}`,
-                    block,
-                    locales: getCleanLocales(locales),
-                    locale,
-                },
-            }
-            createPage(blockPage)
-        })
-    })
-}
-
-const cleanIdString = (id) => id.replace(new RegExp('-', 'g'), '_')
-
-/**
- * Loop over a page's blocks to assemble its page query
- *
- * Arguments: the page's $id
- */
-const getPageQuery = (page) => {
-    const { id, blocks } = page
-    if (!blocks) {
-        return
-    }
-    const queries = _.compact(blocks.map((b) => b.query))
-    if (queries.length === 0) {
-        return
-    }
-    const variables = _.compact(blocks.map((b) => b.queryVariables))
-    const pageQuery = `query page${_.upperFirst(cleanIdString(id))}Query${
-        variables.length > 0 ? `(${variables.join(', ')})` : ''
-    } {
-${indentString(queries.join('\n'), 4)}
-}`
-    return pageQuery
-}
-
-exports.createPages = async ({ graphql, actions: { createPage, createRedirect } }) => {
-    const { flat } = await computeSitemap(rawSitemap)
-
-    const localesResults = await graphql(
-        `
-            ${localesQuery}
-        `
-    )
-    const locales = localesResults.data.surveyApi.locales
-
-    for (const page of flat) {
-        let pageData = {}
-        const context = getPageContext(page)
-
-        // loop over locales
-        for (let index = 0; index < locales.length; index++) {
-            const locale = locales[index]
-            locale.path = `/${locale.id}`
-
-            // console.log('// pageQuery')
-            const pageQuery = getPageQuery(page)
-
-            try {
-                if (pageQuery) {
-                    const queryResults = await graphql(
-                        `
-                            ${pageQuery}
-                        `,
-                        { id: page.id, localeId: locale.id }
-                    )
-                    // console.log('// queryResults')
-                    // console.log(JSON.stringify(queryResults.data, '', 2))
-                    pageData = queryResults.data
-                }
-            } catch (error) {
-                console.log(`// Error while loading data for page ${page.id}`)
-                console.log(pageQuery)
-                console.log(error)
-            }
-
-            const pageObject = {
-                path: getLocalizedPath(page.path, locale),
-                component: path.resolve(`./src/core/pages/PageTemplate.js`),
-                context: {
-                    ...context,
-                    locales: getCleanLocales(locales),
-                    locale,
-                    pageData,
-                    pageQuery, // passed for debugging purposes
-                },
-            }
-            createPage(pageObject)
-
-            // also redirect "naked" paths (whithout a locale) to en-US
-            if (locale.id === 'en-US') {
-                createRedirect({
-                    fromPath: getLocalizedPath(page.path, null),
-                    toPath: getLocalizedPath(page.path, locale),
-                    isPermanent: true,
-                })
-            }
-        }
-
-        createBlockPages(page, context, createPage, locales)
-    }
-}
+exports.createPages = createPagesSingleLoop
 
 // handle 404 page separately
 exports.onCreatePage = async ({ page, graphql, actions }) => {
     const { createPage, deletePage } = actions
-    
+
     // Look for /404/ path
     if (page.path.match(/^\/[a-z]{2}\/404\/$/)) {
         const oldPage = { ...page }
@@ -180,7 +18,7 @@ exports.onCreatePage = async ({ page, graphql, actions }) => {
         // Add page context stubs to avoid throwing errors
         page.context = {
             locales: [],
-            locale: {}
+            locale: {},
         }
         console.log('//404')
         console.log(page)
