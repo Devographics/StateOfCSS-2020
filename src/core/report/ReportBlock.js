f28e9e33f09a3241a292edbec5a2ef5cc39aecff
SachaG <358832+SachaG@users.noreply.github.com>
2020-12-31 10:16:22 +0900

    Move report

--- src/core/report/ReportBlock.js
+++ src/core/report/ReportBlock.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import ReportContents from 'config/report.mdx'
+import ReportContents from 'config/report/en-US/report.mdx'
 import styled from 'styled-components'
 import { mq, color, spacing, fontSize, fontWeight } from 'core/theme'
 import Logo from 'core/components/Logo'
